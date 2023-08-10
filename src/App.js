5428a1d3847225720d268588199e6600a08d1acb
RitsuCloud <chonhinchou@gmail.com>
2023-08-10 16:46:33 -0700

    added a functional header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./NavBar.js";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar/>
       </header>
+      <div className='main-body-container' id="main-id">
+        <p>Hello World</p>
+      </div>
     </div>
   );
 }
 
-export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css';
+
+export default function NavBar(){
+  return(
+    <div className='higher-NavBar'>
+      <div className="name-NavBar">Chon Hin Chou</div>
+      <div className="links-NavBar">
+        <a href="#main-id">Home</a>
+        <a href="https://www.linkedin.com/in/chon-hin-chou/">LinkedIn</a>
+        <a href="https://github.com/RitsuCloud">GitHub</a>
+        <a href="#castle-project-id">Project</a>
+        <a href="#contact-id">Contact</a>
+      </div>
+    </div>
+  )
+}
